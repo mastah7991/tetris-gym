b36937d886503a2808e4498562c0319530b04767
mastah7991 <mastah7991@gmail.com>
2021-01-18 18:13:28 +0100

    All fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='gym_tetris',
-      version='1.0.0',
-      install_requires=['gym', 'pygame', 'numpy', 'keyboard']
-      )
+setup(
+    name="gym_tetris_simple",
+    version="0.0.9",
+    author="mastah7991",
+    author_email="mastah7991@gmail.com",
+    description="Gym env",
+    url="https://github.com/",
+    packages=find_packages(),
+    install_requires=['gym>=0.18.0', 'pygame>=2.0.1', 'numpy>=1.19.1', 'keyboard>=0.13.5'],
+    python_requires='>=3.6',
+)
